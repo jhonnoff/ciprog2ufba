b59a4d976120e441b0847d359d4f908567201d36
jhonnoff <134144416+jhonnoff@users.noreply.github.com>
2023-05-20 11:53:20 -0300

    teste string.cpp
    criando arquivo..

---
+++ string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string nome , resposta;
+  cout<<"Insira seu nome: ";
+  
+  getline(cin, nome);
+  
+  while(resposta!="esquerda" && resposta!="ESQUERDA"&& resposta!="Esquerda"&& resposta!="direita"&& resposta!="DIREITA"&& resposta!="Direita"){
+    
+  cout<< "olá "<< nome << "! Bem vindo ao RPG!"<< endl;
+  
+  cout<< "Você deseja ir para a Direita ou para a Esquerda?" << endl;
+  
+  getline(cin ,resposta);
+  
+  if(resposta=="direita"|| resposta=="DIREITA"|| resposta=="Direita"){
+    
+    cout<<"Vocẽ foi atacado por um Monstro!"<< endl;
+    
+  }
+  if(resposta=="esquerda"|| resposta=="ESQUERDA"|| resposta=="Esquerda"){
+    
+    cout<<"Vocẽ encontrou um tesouro!"<< endl;
+    
+  }
+  if(resposta=="esquerda"|| resposta=="ESQUERDA"|| resposta=="Esquerda"|| resposta=="direita"|| resposta=="DIREITA"|| resposta=="Direita"){
+    
+    cout<<"Obrigado por jogar!!"<< endl;
+    }
+    else
+    cout<<"Caminho inválido, favor retorne ao inicio.. :)"<< endl;
+  }
+
+  return 0;
+}
